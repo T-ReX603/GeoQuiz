b69f87ecd7da1f962e509b50e2bf5b0e4812f10a
USCTeeReX <j.matthews2003@gmail.com>
2023-08-24 12:43:52 +1000

    maybe this will fix it

--- REALQUIZ/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ REALQUIZ/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -5,11 +5,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.example.geoquiz.databinding.ActivityMainBinding
+import android.util.Log
 
+
+
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -21,10 +28,15 @@ class MainActivity : AppCompatActivity() {
 
     private var currentIndex = 0
 
+    private val quizViewModel: QuizViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+
 
 
         binding.trueButton.setOnClickListener { view: View ->
@@ -46,6 +58,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+        }
+
+        override fun onStart() {
+            super.onStart()
+            Log.d(TAG, "onStart() called")
+        }
+        override fun onResume() {
+            super.onResume()
+            Log.d(TAG, "onResume() called")
+        }
+        override fun onPause() {
+            super.onPause()
+            Log.d(TAG, "onPause() called")
+        }
+        override fun onStop() {
+            super.onStop()
+            Log.d(TAG, "onStop() called")
+        }
+        override fun onDestroy() {
+            super.onDestroy()
+            Log.d(TAG, "onDestroy() called")
         }
         private fun updateQuestion() {
             val questionTextResId = questionBank[currentIndex].textResId
